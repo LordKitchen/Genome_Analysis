e0e5b9d86028f0ab95c22940a6051b4b97999b9b
Gabriel André Pettersson <gape9739@rackham4.uppmax.uu.se>
2024-03-29 09:32:16 +0100

    new current version of the trimmed fastqc script

--- data/fastqc/scripts/HP126_DV3_DNA_fastqc_trimmed_script.sh
+++ data/fastqc/scripts/HP126_DV3_DNA_fastqc_trimmed_script.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 #SBATCH -A uppmax2024-2-7
 #SBATCH -M snowy
-#SBATCH --reservation=uppmax2024-2-7_2
 #SBATCH -p core
 #SBATCH -n 2
 #SBATCH -e error.log
 #SBATCH -t 00:10:00
-#SBATCH -J gape_fastq_trimmed
+#SBATCH -J gape_fastqc_trimmed
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user gabrielandre.pettersson.9739@student.uu.se
 
